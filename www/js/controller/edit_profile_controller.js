edbc61f341b7fa0e4848c60c4ce35f4f64b0be93
hirochi <hirosmans@gmail.com>
2015-11-16 00:55:10 -0300

    feat(edit_profile): implements update service

--- www/js/controller/edit_profile_controller.js
+++ www/js/controller/edit_profile_controller.js
@@ -5,25 +5,17 @@
 */
 
 (function() {
-	this.app.controller('EditProfileController', ['$scope', '$ionicPopup', '$state',
-		function($scope, $ionicPopup, $state){
+	this.app.controller('EditProfileController', ['$scope', '$ionicPopup', '$state', 'User', 'user',
+		function($scope, $ionicPopup, $state, User, user){
 	/*
 	=========================================
 		SCOPE DEFINITION
 	=========================================
 	*/
 
-	// RESOLVE USER DATA
-	$scope.user = {
-		firstName: 'hirochi',
-		lastName: 'riveros',
-		email: 'hiro@mifutu.ro',
-		nickname: 'hirosmans',
-		password: '0987654321',
-		passwordConfirm: '0987654321'
-	};
+	$scope.user = user;
+	// TO-DO SEND NEW USER DATA.
 
-	// TODO SEND INFORMATION TO SERVER BY $save() RESTMOD METHOD.
 	$scope.saveChanges = function() {
 		var confirm = $ionicPopup.confirm({
 			title: 'alerta!',
@@ -31,14 +23,35 @@
 		});
 		confirm.then(function(result) {
 			if (result) {
-				// SEND NOTIFICATION TO USER.
-				$state.go('configuration');
+				var updatePromise = User.update($scope.user);
+				updatePromise.then(function(user) {
+					debugger;
+					if (user !== undefined) {
+						$scope.callAlert('error', 'cambios actualizados!');
+					} else {
+						$scope.callAlert('error', 'los cambios no han sido actualizados, favor intentar mas tarde.');
+					};
+				}, function(reason) {
+					debugger;
+					$scope.callAlert('error', 'los cambios no han sido actualizados, favor intentar mas tarde. \n' + JSON.stringifyr(reason));
+				});
+
 			} else {
 				$state.go('configuration');
 			};
 		});
 	};
 
+	$scope.callAlert = function(type, message) {
+		var alert = $ionicPopup.alert({
+			title: type,
+			template: message
+		});
+
+		alert.then(function() {
+			$state.go('configuration');
+		});
+	}
 
 
 
