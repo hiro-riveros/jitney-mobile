36c0248b891918d4f300eb297105641a2cf7cb58
hirochi <hirosmans@gmail.com>
2015-11-29 20:13:26 -0300

    feat(): adding resolve to pre set jitneys, passengers when state is loaded

--- www/js/config/router_config.js
+++ www/js/config/router_config.js
@@ -43,21 +43,54 @@
 		.state('mapJitney', {
 			url: '/map_jitney',
 			templateUrl: 'templates/map_jitney.html',
-			controller: 'MapJitneyController'
+			controller: 'MapJitneyController',
+			resolve: {
+				passengers: ['Passenger', function(Passenger) {
+					return Passenger.getPassengers();
+				}],
+				jitney: ['Jitney', function(Jitney) {
+					return Jitney.getJitney(1);
+				}]
+			}
 		})
 
 		// map view
 		.state('mapPassenger', {
 			url: '/map_passenger',
 			templateUrl: 'templates/map_passenger.html',
-			controller: 'MapController'
+			controller: 'MapController',
+			resolve: {
+				passenger: ['Passenger', function(Passenger) {
+					return Passenger.getPassenger(2);
+				}],
+				jitneys: ['Jitney', function(Jitney) {
+					return Jitney.getJitneys();
+				}]
+			}
 		})
 
-		// configuration view
-		.state('configuration', {
-			url: '/configuration',
-			templateUrl: 'templates/configuration.html',
-			controller: 'ConfigurationController'
+		// passenger configuration view 
+		.state('passengerConfiguration', {
+			url: '/passenger_configuration',
+			templateUrl: 'templates/passenger_configuration.html',
+			controller: 'PassengerConfigurationController',
+			resolve: {
+				passenger: ['Passenger', function(Passenger) {
+					return Passenger.getPassenger(2);
+				}]
+			}
+		})
+		
+		// jitney configuration view
+		.state('jitneyConfiguration', {
+			url: '/jitney_configuration',
+			templateUrl: 'templates/jitney_configuration.html',
+			controller: 'JitneyConfigurationController',
+			resolve: {
+				jitney: ['Jitney', function(Jitney) {
+					return Jitney.getJitney(1);
+				}]
+			}
 		});
 
 		// if none of the above states are matched, use this as the fallback
