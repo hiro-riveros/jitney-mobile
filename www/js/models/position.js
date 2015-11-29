845855348a74805e4ebf532680f1318220507c33
hirochi <hirosmans@gmail.com>
2015-11-29 20:06:19 -0300

    feat(position): adding create and update method to post and put positions

--- www/js/models/position.js
+++ www/js/models/position.js
@@ -16,7 +16,7 @@
 */
 
 (function() {
-	this.app.factory('Position', function($http, $q, ENV, LocalStorageSingletonServices) {
+	this.app.factory('Position', function($http, $q, ENV) {
     return {
     	getPosition: function (userId) {
 				var defer = $q.defer();
@@ -49,30 +49,17 @@
     	create: function (Position) {
 				var defer = $q.defer();
 				$http({
-				  method: 'POST',
-				  url: ENV.API_URL + 'positions',
-				  params:{
-				  	user_id: Position.userId,
-				  	latitude: Position.latitude,
-				  	longitude: Position.longitude,
-				  	perimeter: Position.perimeter
-				  }
-				}).then(function(Position) {
-					if (Position.data !== undefined && Position.data !== '') {
-							var position = {
-								id: Position.data.id,
-								userId: Position.data.user_id,
-						  	latitude: Position.data.latitude,
-						  	longitude: Position.data.longitude,
-						  	perimeter: Position.data.perimeter
-							};
-							defer.resolve(position);
-					} else {
-						return {
-							error: 'lo sentimos no hemos podido crear tu cuenta'
-						};
-					};
-				}, function(reason) {
+		    	url: ENV.API_URL + 'positions',
+		    	method: 'POST',
+		    	params: { 
+						user_id: Position.userId,
+					  latitude: Position.latitude,
+					  longitude: Position.longitude,
+					  perimeter: Position.perimeter
+					}
+				}).then(function(position) {
+					defer.resolve(position.data);
+				}, function (reason) {
 					defer.reject(reason.data);
 				});
 				return defer.promise;
